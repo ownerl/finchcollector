b939e9a56fbd8620f9b81ccbe744d14620bfb27c
Abay <ibrayev.abay@gmail.com>
2023-12-11 10:55:54 -0700

    l3

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=231)
+    color = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    favourite_anime = models.CharField(max_length=100)
