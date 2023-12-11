f25dbf86c5560422f9eb1e85bea169e24fbb03b6
Abay <ibrayev.abay@gmail.com>
2023-12-10 22:43:50 -0700

    commit 3, base and view

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
+finches = [
+    {'name': 'Vader', 'breed': 'too fat', 'description': 'fat with the force', 'age': 42},
+    {'name': 'Luke', 'breed': 'too skinny', 'description': 'skinny with the force', 'age': 20},
+]
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
